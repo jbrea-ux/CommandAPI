159d6ec5dc64b37371167b5f9115f42b6ba804da
Jova <jbreadev@gmail.com>
2021-10-12 22:13:48 -0400

    Added Command Model to API Project

--- src/CommandAPI/Models/Command.cs
+++ src/CommandAPI/Models/Command.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace CommandAPI.Model
+namespace CommandAPI.Models
 {
     public class Command
     {
